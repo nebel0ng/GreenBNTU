ff6607dabc9594fea037e32a7c7019f56152a2aa
Юля Дорощёнок <96148784+nebel0ng@users.noreply.github.com>
2022-06-09 10:24:31 +0300

    Update LoginWindowController.cs

--- GreenHSE.Design/htmlControllers/LoginWindowController.cs
+++ GreenHSE.Design/htmlControllers/LoginWindowController.cs
@@ -1,27 +1,28 @@
-﻿using GreenBNTU.Design.Interfaces;
-using GreenBNTU.Design.Repository;
+using GreenBNTU.Design.Interfaces;
 using GreenBNTU.Design.Models;
 using Microsoft.AspNetCore.Mvc;
-using Microsoft.EntityFrameworkCore;
 
 namespace GreenBNTU.Design.htmlControllers
 {
     public class LoginWindowController : Controller
     {
+        private readonly IAllAdmins _AllAdmins;
+
+        public LoginWindowController(IAllAdmins allAdmins)
+        {
+            _AllAdmins = allAdmins;
+        }
         public ActionResult LoginWindow()
         {
             return View("LoginWindow");
         }
 
         public ViewResult Login(Admin admin)
-        {
-            var contextOptions = new DbContextOptions<Context>();
-            var context = new Context(contextOptions);
-            var adminRep = new AdminRepository(context);
+        {           
             var currentAdmin = new Admin();
-            if (adminRep.AdminExists(admin.Login, admin.Password))
+            if (_AllAdmins.AdminExists(admin.Login, admin.Password))
             {
-                currentAdmin = adminRep.LoginAdmin(admin.Login, admin.Password);
+                currentAdmin = _AllAdmins.LoginAdmin(admin.Login, admin.Password);
             }
             while(currentAdmin.Password == null)
             {
