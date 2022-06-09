284abe14b61c3733d5b76d85fe8eec8de2c18039
Юля Дорощёнок <96148784+nebel0ng@users.noreply.github.com>
2022-06-09 10:22:44 +0300

    Update GoogleMapsController.cs

--- GreenHSE.Design/htmlControllers/GoogleMapsController.cs
+++ GreenHSE.Design/htmlControllers/GoogleMapsController.cs
@@ -1,32 +1,31 @@
-﻿using GreenBNTU.Design.Models;
-using GreenBNTU.Design.Repository;
+using GreenBNTU.Design.Interfaces;
+using GreenBNTU.Design.Models;
 using Microsoft.AspNetCore.Mvc;
-using Microsoft.EntityFrameworkCore;
 
 namespace GreenBNTU.Design.htmlControllers
 {
     public class GoogleMapsController : Controller
     {
+        private readonly IAllLocations _allLocations;
+        private readonly IAllObjects _allObjects;
+        public GoogleMapsController(IAllLocations allLocations, IAllObjects allObjects)
+        {
+            _allLocations = allLocations;
+            _allObjects = allObjects;
+        }
+        
         public ActionResult GoogleMap()
         {
-            var contextOptions = new DbContextOptions<Context>();
-            var context = new Context(contextOptions);
-            var recObjectRepository = new RecObjectRepository(context);
-            var recObjects = recObjectRepository.GetAllRecObjects();
-            var locationRep = new LocationRepository(context);
-            var locations = locationRep.GetAllLocations();
-            return View(locations);
+            var recObjects = _allObjects.GetAllRecObjects();
+            var locations = _allLocations.GetAllLocations();
+            return View(new Tuple<ICollection<Location>, ICollection<RecyclableObject>>(locations, recObjects));
         }
 
         public ActionResult GoogleMapLogged()
         {
-            var contextOptions = new DbContextOptions<Context>();
-            var context = new Context(contextOptions);
-            var recObjectRepository = new RecObjectRepository(context);
-            var recObjects = recObjectRepository.GetAllRecObjects();
-            var locationRep = new LocationRepository(context);
-            var locations = locationRep.GetAllLocations();
-            return View(locations);
+            var recObjects = _allObjects.GetAllRecObjects();
+            var locations = _allLocations.GetAllLocations();
+            return View(new Tuple<ICollection<Location>, ICollection<RecyclableObject>>(locations, recObjects));
         }
 
     }
